100620087d2615cfcb0775246f63d4e34437495b
chinmay <chinmay2997@gmail.com>
2019-03-18 01:42:14 +0530

    Initial commit

---
+++ app/src/main/java/com/bmitestbna/bmiapp/Underweight.java
@@ -0,0 +1,13 @@
+package com.bmitestbna.bmiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Underweight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_underweight);
+    }
+}
